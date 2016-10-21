271ba1ae60a34d43cb6cdc9046acdb0ac7b6c369
Vishnu Raj <get_vichu@yahoo.com>
2016-10-21 16:46:49 +0530

    Recommiting Bandits.jl

---
+++ src/Bandits.jl
@@ -0,0 +1,37 @@
+module Bandits
+
+module Algorithms
+    using Distributions
+    #--------------------------- Import Algorithms Here ---------------------------#
+    include( "algorithms/BanditAlgorithmBase.jl" )
+    include( "algorithms/UCB.jl" )
+    include( "algorithms/epsGreedy.jl" )
+end
+
+module Arms
+    using Distributions
+    #--------------------------- Import Arm Models Here ---------------------------#
+    include( "armModels/BanditArmBase.jl" )
+    include( "armModels/Bernoulli.jl" )
+    include( "armModels/Normal.jl" )
+end
+
+module Experiments
+    using Distributions
+    include( "algorithms/BanditAlgorithmBase.jl" )
+    include( "armModels/BanditArmBase.jl" )
+    include( "experiments/BanditExpBase.jl" )
+    include( "experiments/Compare.jl" )
+end
+
+#--------------------------- Import Experiments Here --------------------------#
+
+export
+    # Export algorithms
+    Algorithms,
+    # Export Arm models
+    Arms,
+    # Export Experiments
+    Experiments
+
+end # module
