05a607447401fb5e37d55bbfdd64c608094161cb
Vishnu Raj <get_vichu@yahoo.com>
2016-12-18 11:02:37 +0530

    Added eps_n Greedy

--- examples/02_experimentDemo.jl
+++ examples/02_experimentDemo.jl
@@ -4,25 +4,30 @@ using Bandits
 import PyPlot
 
 bandit  = [
-    Arms.Bernoulli( 0.12 ),
-    Arms.Bernoulli( 0.90 ),
-    Arms.Bernoulli( 0.45 ),
-    Arms.Bernoulli( 0.63 )
+    # Arms.Bernoulli( 0.12 ),
+    # Arms.Bernoulli( 0.90 ),
+    # Arms.Bernoulli( 0.45 ),
+    # Arms.Bernoulli( 0.63 )
     # Arms.Normal( 0.36, 1.00 ),
     # Arms.Normal( 0.20, 1.00 ),
-    # Arms.Beta( 0.60, 0.40 ),
-    # Arms.Beta( 0.70, 0.20 )
+    Arms.Beta( 0.60, 0.40 ),
+    Arms.Beta( 0.70, 0.20 ),
+    Arms.Beta( 0.25, 0.30 ),
+    Arms.Beta( 0.15, 0.76 )
 ]
 
 noOfArms = size( bandit, 1 )
 
 testAlgs = [
     Algorithms.epsGreedy( noOfArms, 0.05 ),
-    # Algorithms.epsGreedy( noOfArms, 0.10 ),
+    Algorithms.epsGreedy( noOfArms, 1.00 ),
+    Algorithms.epsNGreedy( noOfArms, 5, 0.05 ),
+    Algorithms.epsNGreedy( noOfArms, 1/noOfArms, 1.0 ),
+    Algorithms.epsNGreedy( noOfArms )
     # Algorithms.EXP3( noOfArms, 0.05 ),
-    Algorithms.EXP3( noOfArms, 0.10 ),
-    Algorithms.UCB1( noOfArms ),
-    Algorithms.TS( noOfArms )
+    # Algorithms.EXP3( noOfArms, 0.10 ),
+    # Algorithms.UCB1( noOfArms ),
+    # Algorithms.TS( noOfArms )
 ]
 
 exp1 = Experiments.Compare( bandit, testAlgs )
--- src/algorithms/epsGreedy.jl
+++ src/algorithms/epsGreedy.jl
@@ -58,3 +58,90 @@ end
 function info_str( agent::epsGreedy )
     return @sprintf( "ϵ-Greedy (ϵ = %4.3f)", agent.ϵ )
 end
+
+
+"""
+    ϵ_n Greedy Implementation
+    Based on Auer, P., Bianchi, N. C., & Fischer, P. (2002). Finite time analysis of the multiarmed bandit problem. Machine Learning, 47, 235–256.
+"""
+
+type epsNGreedy <: BanditAlgorithmBase
+    noOfArms::Int64
+    noOfSteps::Int64
+    lastPlayedArm::Int64
+
+    param_c::Float64
+    param_d::Float64
+    ϵ::Float64
+
+    cummReward::Vector{Float64}
+    count::Vector{Int64}
+    avgValue::Vector{Float64}
+
+    function epsNGreedy( noOfArms::Int64, param_c::Real, param_d::Float64 )
+        new( noOfArms,
+             0,
+             0,
+             param_c,
+             param_d,
+             1,
+             zeros(Float64,noOfArms),
+             zeros(Int64,noOfArms),
+             zeros(Float64,noOfArms)
+        )
+    end
+
+    function epsNGreedy( noOfArms::Int64 )
+        new( noOfArms,
+             0,
+             0,
+             1/noOfArms,
+             1,
+             1,
+             zeros(Float64,noOfArms),
+             zeros(Int64,noOfArms),
+             zeros(Float64,noOfArms)
+        )
+    end
+end
+
+function getArmIndex( agent::epsNGreedy )
+    if any(agent.count.==0)
+        agent.lastPlayedArm = find(agent.count.==0)[1]
+    else
+        if rand() > agent.ϵ
+            agent.lastPlayedArm = findmax(agent.avgValue)[2]
+        else
+            agent.lastPlayedArm = rand(1:agent.noOfArms)
+        end
+    end
+    return agent.lastPlayedArm
+end
+
+function updateReward( agent::epsNGreedy, r::Real )
+    # Book keeping
+    agent.cummReward[agent.lastPlayedArm] += r
+    agent.count[agent.lastPlayedArm] += 1
+    agent.noOfSteps += 1
+
+    # Update the observed reward to the corresponding arm
+    agent.avgValue[agent.lastPlayedArm] = agent.cummReward[agent.lastPlayedArm] ./
+                                            agent.count[agent.lastPlayedArm]
+
+    # Also change the exploration rate
+    agent.ϵ     = min( 1, (agent.param_c*agent.noOfArms)/(agent.param_d*agent.noOfSteps) )
+end
+
+function reset( agent::epsNGreedy )
+    agent.noOfSteps     = 0
+    agent.lastPlayedArm = 0
+    agent.ϵ             = 1
+
+    agent.cummReward    = zeros( Float64, agent.noOfArms )
+    agent.count         = zeros( Int64, agent.noOfArms )
+    agent.avgValue      = zeros( Float64, agent.noOfArms )
+end
+
+function info_str( agent::epsNGreedy )
+    return @sprintf( "ϵ_N - Greedy (c = %4.3f, d = %4.3f)", agent.param_c, agent.param_d )
+end
