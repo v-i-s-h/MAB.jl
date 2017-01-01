ac281fcba726c86fd8f051bfca3e0cf2ab9795da
Vishnu Raj <get_vichu@yahoo.com>
2017-01-01 19:32:48 +0530

    Modification for UCB Normal

--- examples/02_experimentDemo.jl
+++ examples/02_experimentDemo.jl
@@ -8,12 +8,14 @@ bandit  = [
     # Arms.Bernoulli( 0.90 ),
     # Arms.Bernoulli( 0.45 ),
     # Arms.Bernoulli( 0.63 )
-    # Arms.Normal( 0.36, 1.00 ),
-    # Arms.Normal( 0.20, 1.00 ),
-    Arms.Beta( 0.60, 0.40 ),
-    Arms.Beta( 0.70, 0.20 ),
-    Arms.Beta( 0.25, 0.30 ),
-    Arms.Beta( 0.15, 0.76 )
+    Arms.Normal( 0.36, 1.00 ),
+    Arms.Normal( 0.20, 1.00 ),
+    Arms.Normal( 0.81, 1.00 ),
+    Arms.Normal( 0.56, 1.00 ),
+    # Arms.Beta( 0.60, 0.40 ),
+    # Arms.Beta( 0.70, 0.20 ),
+    # Arms.Beta( 0.25, 0.30 ),
+    # Arms.Beta( 0.15, 0.76 )
 ]
 
 noOfArms = size( bandit, 1 )
@@ -21,20 +23,21 @@ noOfArms = size( bandit, 1 )
 testAlgs = [
     Algorithms.epsGreedy( noOfArms, 0.05 ),
     # Algorithms.epsGreedy( noOfArms, 1.00 ),
-    Algorithms.epsNGreedy( noOfArms, 5, 0.05 ),
+    # Algorithms.epsNGreedy( noOfArms, 5, 0.05 ),
     # Algorithms.epsNGreedy( noOfArms, 1/noOfArms, 1.0 ),
     # Algorithms.epsNGreedy( noOfArms )
     # Algorithms.EXP3( noOfArms, 0.05 ),
     # Algorithms.EXP3( noOfArms, 0.10 ),
-    # Algorithms.UCB1( noOfArms ),
-    Algorithms.TS( noOfArms ),
-    Algorithms.DynamicTS( noOfArms, 50 )
+    Algorithms.UCB1( noOfArms ),
+    # Algorithms.TS( noOfArms ),
+    # Algorithms.DynamicTS( noOfArms, 50 )
+    Algorithms.UCBNormal( noOfArms )
 ]
 
 exp1 = Experiments.Compare( bandit, testAlgs )
 # run
 noOfRounds      = 2000
-noOfTimeSteps   = 500
+noOfTimeSteps   = 2500
 result = Experiments.run( exp1, noOfTimeSteps, noOfRounds )
 
 fig = PyPlot.figure()
