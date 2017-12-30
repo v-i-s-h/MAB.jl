7990d79e23bc698121d5ef534f44905bb8d00707
autodocs <autodocs>
2017-12-30 10:57:04 +0000

    build based on 7fe6e88

---
+++ stable/search_index.js
@@ -0,0 +1,163 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#MAB",
+    "page": "Home",
+    "title": "MAB",
+    "category": "Module",
+    "text": "MAB.jl is a package for experimenting with multi arm bandit algorithms. This package comes with different MAB algorithms, various arm models and a module to reproduce/evaluate your own algorithms in different scenarios/experiments which are already available in literature.\n\nThis package is divied into three main modules\n\nAlgorithms\nArmModels\nExperiments\n\n\n\n"
+},
+
+{
+    "location": "#MAB.jl-1",
+    "page": "Home",
+    "title": "MAB.jl",
+    "category": "section",
+    "text": "CurrentModule = MABA Julia Package for Experimenting with Multi Arm BanditsMAB"
+},
+
+{
+    "location": "#Manual-1",
+    "page": "Home",
+    "title": "Manual",
+    "category": "section",
+    "text": "Pages = [\n            \"manual/guide.md\",\n            \"manual/contributing.md\",\n            \"manual/ack.md\"\n]"
+},
+
+{
+    "location": "#Package-Outline-1",
+    "page": "Home",
+    "title": "Package Outline",
+    "category": "section",
+    "text": "Pages = [\n            \"library/common_api.md\",\n            \"library/algorithms.md\",\n            \"library/arm_models.md\",\n            \"library/experiments.md\"\n]"
+},
+
+{
+    "location": "manual/guide/#",
+    "page": "Package Guide",
+    "title": "Package Guide",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "manual/guide/#Package-Guide-1",
+    "page": "Package Guide",
+    "title": "Package Guide",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "manual/guide/#Installing-MAB.jl-package-1",
+    "page": "Package Guide",
+    "title": "Installing MAB.jl package",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "manual/guide/#Usage-1",
+    "page": "Package Guide",
+    "title": "Usage",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "manual/guide/#Accessing-documentation-from-REPL-1",
+    "page": "Package Guide",
+    "title": "Accessing documentation from REPL",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "manual/contributing/#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "manual/ack/#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "library/common_api/#",
+    "page": "Common API",
+    "title": "Common API",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "library/common_api/#Common-API-1",
+    "page": "Common API",
+    "title": "Common API",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "library/algorithms/#",
+    "page": "Algorithms",
+    "title": "Algorithms",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "library/algorithms/#Algorithms-1",
+    "page": "Algorithms",
+    "title": "Algorithms",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "library/arm_models/#",
+    "page": "Arm Models",
+    "title": "Arm Models",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "library/arm_models/#Arm-Models-1",
+    "page": "Arm Models",
+    "title": "Arm Models",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "library/experiments/#",
+    "page": "Experiments",
+    "title": "Experiments",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "library/experiments/#Experiments-1",
+    "page": "Experiments",
+    "title": "Experiments",
+    "category": "section",
+    "text": ""
+},
+
+]}
